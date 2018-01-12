513d9d9bea0bdc383009643a536feb23016e6ba0
Robert Jakes <robert.jakes@iongroup.com>
2018-01-12 13:56:24 +0100

    add placeholder title values for some recipes

--- src/app/services/recipe-fetcher.service.ts
+++ src/app/services/recipe-fetcher.service.ts
@@ -25,28 +25,28 @@ const MOCK_IMAGES =[
   {
     "albumId": 1,
     "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
+    "title": "Recipe 1 title",
     "url": "http://placehold.it/600/92c952",
     "thumbnailUrl": "http://placehold.it/150/92c952"
   },
   {
     "albumId": 1,
     "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
+    "title": "Recipe 2 title",
     "url": "http://placehold.it/600/771796",
     "thumbnailUrl": "http://placehold.it/150/771796"
   },
   {
     "albumId": 1,
     "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
+    "title": "Recipe 3 title",
     "url": "http://placehold.it/600/24f355",
     "thumbnailUrl": "http://placehold.it/150/24f355"
   },
   {
     "albumId": 1,
     "id": 4,
-    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    "title": "Recipe 4 title",
     "url": "http://placehold.it/600/d32776",
     "thumbnailUrl": "http://placehold.it/150/d32776"
   },
