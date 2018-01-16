00ec3d399a5ada252710c6c6185e78f2ef75133f
Robert Jakeš <jakesrobert@gmail.com>
2018-01-16 15:45:46 +0100

    refactoring 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
-import { appRoutes } from '../routes'
 
-import { RecipeFetcherService } from './services/recipe-fetcher.service';
+import { RecipeFetcherService } from './shared/services/recipeRepository/recipe-fetcher.service';
 import { FooterComponent } from './components/footer/footer.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { ProtocolComponent } from './components/recipe/protocol/protocol.component';
@@ -17,25 +23,29 @@ import { CommentsComponent } from './components/recipe/comments/comments.compone
 import { OverviewComponent } from './components/recipe/overview/overview.component';
 import { IngredientsComponent } from './components/recipe/ingredients/ingredients.component';
 
+export function createTranslateLoader(http: HttpClient) {
+  // for development
+  // return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-5/master/dist/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    GalleryComponent,
-    SearchbarComponent,
-    FooterComponent,
-    RecipeComponent,
-    ProtocolComponent,
-    CommentsComponent,
-    OverviewComponent,
-    IngredientsComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    CommonModule,
     BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes)
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient]
+        }
+    }),
+    AppRoutingModule
   ],
-  providers: [RecipeFetcherService],
+  providers: [RecipeFetcherService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/services/index.ts
@@ -0,0 +1,1 @@
+export * from './recipeRepository/recipe-fetcher.service';
